2bfd4081ff3f1b583f67739e425a8bf7bb4421e0
Erich Dingeldein <erich.dingeldein@und.edu>
2017-10-04 12:47:06 -0500

    First Commit

---
+++ Node.java
@@ -0,0 +1,14 @@
+public class Node {
+	
+	public int key;
+	public int depth;
+	public int height;
+	public Node parent;
+	public Node left;
+	public Node right;
+
+	public Node(int key) {
+		
+	}
+
+}
